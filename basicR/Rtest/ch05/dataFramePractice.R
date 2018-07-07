9aaf03e5fc80feb2cefdd00b3cda90a8841cb0cd
jinhyeok <persona5173@gmail.com>
2018-07-07 20:07:35 +0900

    Feat: basicR Lesson5 add Rfiles

---
+++ basicR/Rtest/ch05/dataFramePractice.R
@@ -0,0 +1,4 @@
+stuOneData <- list(Number=1, Name="cheolsoo", scoKor=60, scoEng = 70, grade="D")
+stuOneData$scoEng <- 60
+stuOneData$scoSci <- 90
+stuOneData$grade <- NULL
---
+++ basicR/Rtest/ch05/listNaming.R
@@ -0,0 +1,3 @@
+stuOneData <- list(number=1,Name="cheolsoo", scoKor=60, scoEng=50, scoMat=70, grade="D")
+stuOneData1 <- list(1,"cheolsoo", 60, 50, 70, "D")
+print(stuOneData)
